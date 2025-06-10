7a984b6f59672253b254b859b0baee37d79745ea
Imran-Butun <imranbayrampro@gmail.com>
2025-06-10 14:18:26 +0200

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio-scolaire/', // Remplacez par le nom de votre dépôt GitHub
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
